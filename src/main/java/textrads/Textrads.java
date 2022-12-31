c2a3068bdd8847d3ad5c8aea599798ec94e09965
Michael Birken <o__1@hotmail.com>
2022-12-31 11:38:19 -0500

    creating

--- src/main/java/textrads/Textrads.java
+++ src/main/java/textrads/Textrads.java
@@ -2,7 +2,7 @@ package textrads;
 
 public class Textrads {
 
-    public static void main(String[] args) {
+    public static void main(final String... args) {
         System.out.println("Hello World!");
     }
 }
