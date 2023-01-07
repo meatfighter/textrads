7e4d7ff5c411fc2711b1ea9aa450cdcff56356ec
Michael Birken <o__1@hotmail.com>
2023-01-07 11:44:43 -0500

    tests

--- src/main/java/textrads/Textrads.java
+++ src/main/java/textrads/Textrads.java
@@ -1,10 +1,43 @@
 package textrads;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
 public class Textrads {
     
     public void launch() throws Exception {
-        
-
+        try (final Screen screen = new TerminalScreen(new DefaultTerminalFactory().createTerminal())) {
+            screen.startScreen();
+            screen.setCursorPosition(null); // turn off cursor
+            
+            final TextGraphics g = screen.newTextGraphics();
+            TerminalSize size = screen.getTerminalSize();
+            
+            while (true) {
+                final TerminalSize s = screen.doResizeIfNecessary​();
+                if (s != null) {
+                    size = s;
+                }
+                final KeyStroke keyStroke = screen.pollInput();
+                if (keyStroke != null) {
+                    if (keyStroke.getKeyType() == KeyType.Escape) {
+                        break;
+                    } else if (keyStroke.getKeyType() == KeyType.Character) {
+                        g.setForegroundColor(TextColor.ANSI.GREEN_BRIGHT);
+                        g.putString(size.getColumns() / 2, size.getRows() / 2, 
+                                Character.toString(keyStroke.getCharacter()));
+                    }
+                }
+                screen.refresh(); 
+                Thread.sleep(15);
+            }            
+        } 
     }
     
 
