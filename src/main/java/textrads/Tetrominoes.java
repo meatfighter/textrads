8b6c0a851838762a93b1114ff6ff3f85c30b6b2c
Michael Birken <o__1@hotmail.com>
2023-01-08 17:07:01 -0500

    draw big

--- src/main/java/textrads/Tetrominoes.java
+++ src/main/java/textrads/Tetrominoes.java
@@ -3,11 +3,11 @@ package textrads;
 public class Tetrominoes {
     
     // T J Z O S L I
+    
+    public static final int[][] TD = { { 0, 0 }, { -1, 0 }, { 0,  1 }, { 1, 0 } };
+    public static final int[][] TL = { { 0, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+    public static final int[][] TU = { { 0, 0 }, { -1, 0 }, { 0, -1 }, { 1, 0 } };
+    public static final int[][] TR = { { 0, 0 }, {  1, 0 }, { 0, -1 }, { 0, 1 } };
 
-    public static final int[][][] T = {
-        { { 0, 0 }, { -1, 0 }, { 0,  1 }, { 1, 0 }, }, // td
-        { { 0, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 }, }, // tl
-        { { 0, 0 }, { -1, 0 }, { 0, -1 }, { 1, 0 }, }, // tu
-        { { 0, 0 }, {  1, 0 }, { 0, -1 }, { 0, 1 }, }, // tr
-    };
+    public static final int[][][] T = { TD, TL, TU, TR };    
 }
