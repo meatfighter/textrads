f6c3d9fff559c8a2c2269cced1c323fda4f1d3b5
Michael Birken <o__1@hotmail.com>
2023-01-15 16:51:53 -0500

    drop

--- src/main/java/textrads/MonoGameState.java
+++ src/main/java/textrads/MonoGameState.java
@@ -36,7 +36,7 @@ public class MonoGameState {
     
     private int attackRows;
     private int score;
-    private int level = 10;
+    private int level = 20;
     private int lines;
     
     private int tetrominoType;
