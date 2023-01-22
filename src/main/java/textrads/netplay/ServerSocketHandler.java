b4997248c97e055e7bf25738820eb7d2c7299ebe
Michael Birken <o__1@hotmail.com>
2023-01-22 17:44:24 -0500

    netplay

--- src/main/java/textrads/netplay/ServerSocketHandler.java
+++ src/main/java/textrads/netplay/ServerSocketHandler.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.List;
-import textrads.GameState;
 
 public class ServerSocketHandler {
     
@@ -27,44 +25,13 @@ public class ServerSocketHandler {
     private void handleInput() {
         try {
             while (true) {
-                switch (in.read()) {
-                    case Command.HEARTBEAT:
-                        break;
-                    case Command.STATE:
-                        break;
-                    case Command.EVENTS:
-                        break;
-                }
-            }
-        } catch (final Exception ignored) {            
+
+            }         
         } finally {
             close();
         }
     }
-    
-    public void sendHeartbeat() {
-        synchronized (out) {
-            try {
-                out.write(Command.HEARTBEAT);
-                out.flush();
-            } catch (final IOException e) {
-                close();
-            }
-        }
-    }
-    
-    public void sendState(final GameState state) {
-        synchronized (out) {
-            
-        }
-    }
-    
-    public void sendEvents(final List<Integer>[] events) {
-        synchronized (out) {
-            
-        }
-    }
-    
+        
     public void close() {
         running = false;
         
