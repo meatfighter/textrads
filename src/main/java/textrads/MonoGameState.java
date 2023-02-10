beca30cd9eac20bd1fa14c7faeb59adb39416901
Michael Birken <michael.birken@factset.com>
2023-02-10 17:11:32 -0500

    ai

--- src/main/java/textrads/MonoGameState.java
+++ src/main/java/textrads/MonoGameState.java
@@ -109,7 +109,7 @@ public class MonoGameState implements Serializable {
         
         attackRows = 0;
         score = 0;
-        level = 10; // TODO TESTING
+        level = 0; // TODO TESTING
         lines = 0;
         tetrominoType = 0;
         tetrominoRotation = 0;
