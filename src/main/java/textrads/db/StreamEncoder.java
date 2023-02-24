f4c4b2347be827a7e26aabdf40ff320252a80de6
Michael Birken <michael.birken@factset.com>
2023-02-24 11:23:14 -0500

    scores

--- src/main/java/textrads/db/StreamEncoder.java
+++ src/main/java/textrads/db/StreamEncoder.java
@@ -1,5 +1,8 @@
 package textrads.db;
 
-public interface StreamEncoder {
-    
+import java.io.IOException;
+import java.io.OutputStream;
+
+public interface StreamEncoder<T> {
+    void encode(T object, OutputStream out) throws IOException;
 }
